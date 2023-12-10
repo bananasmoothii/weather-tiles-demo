158ea95a1a8d0d38b4775687e6f1cdf29f9fcc6b
Bananasmoothii <45853225+bananasmoothii@users.noreply.github.com>
2023-12-10 11:28:32 +0100

    added phone in contact details

--- src/components/ContactInfo.vue
+++ src/components/ContactInfo.vue
@@ -1,23 +1,27 @@
 <script lang="ts">
 import { defineComponent } from "vue";
-import { AtSymbolIcon, MapPinIcon } from "@heroicons/vue/24/outline";
+import { AtSymbolIcon, MapPinIcon, PhoneIcon } from "@heroicons/vue/24/outline";
 import IconAndText from "@/components/util/IconAndText.vue";
 
 export default defineComponent({
   name: "ContactInfo",
-  components: { IconAndText, MapPinIcon, AtSymbolIcon },
+  components: { IconAndText, MapPinIcon, AtSymbolIcon, PhoneIcon },
 });
 </script>
 
 <template>
   <div class="px-4 py-2">
-    <IconAndText>
+    <IconAndText class="mt-1">
       <MapPinIcon />
-      <p class="m-0">Number 17<br />Cherry Tree Lane</p>
+      <span>Number 17<br />Cherry Tree Lane</span>
     </IconAndText>
-    <IconAndText>
+    <IconAndText class="mt-1">
       <AtSymbolIcon />
       <a href="mailto://mary@poppins.co.uk">mary@poppins.co.uk</a>
+    </IconAndText>
+    <IconAndText class="mt-1">
+      <PhoneIcon />
+      <a href="tel:+44 1234 567890">+44 1234 567890</a>
     </IconAndText>
   </div>
 </template>
