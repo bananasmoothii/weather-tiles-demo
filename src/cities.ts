fe7074bcd56e7e69ed377b548891d16f57905936
Bananasmoothii <45853225+bananasmoothii@users.noreply.github.com>
2023-12-10 15:36:07 +0100

    using local storage to store cities

--- src/cities.ts
+++ src/cities.ts
@@ -1,4 +1,4 @@
-import { ref } from "vue";
+import { ref, watch } from "vue";
 
 export type City = {
   name: string;
@@ -6,31 +6,36 @@ export type City = {
   localized?: string;
 };
 
-export const cities = ref(
-  [
-    "Paris",
-    "Grenoble",
-    "Vienna",
-    "Hamburg",
-    "Berlin",
-    "London",
-    "New York",
-    "Tokyo",
-    "Moscow",
-    "Copenhagen",
-    "Stockholm",
-    "Amsterdam",
-    "Montreal",
-    "Beijing",
-    "Sydney",
-    "Cairo",
-    "Madrid",
-    "Lisbon",
-    "Athens",
-    "Dubai",
-  ].map((name) => ({ name: name, randomId: Math.random() })) as City[],
-);
+let defaultCities: City[] = [
+  "Paris",
+  "Grenoble",
+  "Vienna",
+  "Hamburg",
+  "Berlin",
+  "London",
+  "New York",
+  "Tokyo",
+  "Moscow",
+  "Copenhagen",
+  "Stockholm",
+  "Amsterdam",
+  "Montreal",
+  "Beijing",
+  "Sydney",
+  "Cairo",
+  "Madrid",
+  "Lisbon",
+  "Athens",
+  "Dubai",
+].map((name) => ({ name: name, randomId: Math.random() }));
+
+let localCities = localStorage.getItem("cities");
+export const cities = ref(localCities ? JSON.parse(localCities) : defaultCities);
 
 export function nameToCity(name: string): City {
   return { name: name, randomId: Math.random() };
 }
+
+watch(cities, (newCities) => {
+  localStorage.setItem("cities", JSON.stringify(newCities));
+});
